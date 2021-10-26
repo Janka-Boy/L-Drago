4455ffe9df578768c786ba453bde7bd7e3c5d90d
Janka <jankadiscord@gmail.com>
2021-10-26 22:55:38 +0300

    www

--- tex.py
+++ tex.py
@@ -1,16 +1,12 @@
-import hikari
+import lightbulb
+from secret import token
 
-bot = hikari.GatewayBot(token="ODg4MDk5MzkzMDY3MTY3Nzg2.YUNxAw.o984jSjJFKYQ7R1B_wBw1HFdOPQ")
+bot = lightbulb.Bot(token=token, prefix='1')
 
-@bot.listen()
-async def ping(event: hikari.GuildMessageCreateEvent) -> None:
-    # If a non-bot user sends a message "hk.ping", respond with "Pong!"
-    # We check there is actually content first, if no message content exists,
-    # we would get `None' here.
-    if event.is_bot or not event.content:
-        return
 
-    if event.content.startswith("hk.ping"):
-        await event.message.respond("Pong!")
+@bot.command()
+async def ping(ctx):
+    await ctx.respond("Pong!")
+
 
 bot.run()
