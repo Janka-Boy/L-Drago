371833b3628380d0b740da8ec5708b6d573045cc
Janka <jankadiscord@gmail.com>
2021-10-30 22:25:54 +0300

    guildinfo guild: was made(when)

--- coms/guildinfo.py
+++ coms/guildinfo.py
@@ -1,22 +1,19 @@
 import hikari
 import lightbulb
-
+from datetime import datetime
 
 
 class guild(lightbulb.SlashCommand):
     description: str = "Get info about guild"
     async def callback(self, ctx: lightbulb.SlashCommandContext) -> None:
-        guild_createdAt= ctx.guild_id.created_at.date().strftime('%Y/%m/%d')
         guild = ctx.get_guild()
-        guild_embed = hikari.Embed(description=f'**{guild}**', color='#5930D0')
-        hikari.Embed.add_field(self=guild_embed, name='♤ Guild info:', value=f'**Guild name:** {guild}\n**ID:** {guild.id}\n**Guild was made:** {guild_createdAt}\n**Members:** {guild.member_count}')
+        guild_createdAt= int(guild.created_at.timestamp())
+        guild_embed = hikari.Embed(description=f'**{guild}**', color='#5930D0', timestamp=datetime.now().astimezone())
+        hikari.Embed.add_field(self=guild_embed, name='♤ Guild info:', value=f'**Guild name:** {guild}\n**ID:** {guild.id}\n**Guild was made:** <t:{guild_createdAt}:d> (<t:{guild_createdAt}:R>)\n**Members:** {guild.member_count}')
         guild_embed.set_thumbnail(guild.icon_url)
-        print(guild_createdAt, guild)
+        #print(guild_createdAt, guild)
         await ctx.respond(guild_embed)
 
-        
-        
-        await ctx.respond()
 
 
 def load(bot: lightbulb.Bot) -> None:
