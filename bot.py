341909981e7f17ff444845624be47c14d5a4efba
Janka <jankadiscord@gmail.com>
2021-10-31 13:31:21 +0200

    deleted usles crap

--- bot.py
+++ bot.py
@@ -13,13 +13,6 @@ bot = lightbulb.Bot(
     ignore_bots=True,
 )
 
-
-#class L_Drago(lightbulb.Bot):
-  #  def __init__(self, token):#self, token: str, *, prefix: typing.Optional[prefix_T] = None, insensitive_commands: bool = False, ignore_bots: bool = True, owner_ids: typing.Iterable[int] = ..., help_class: typing.Type[help_.HelpCommand] = ..., delete_unbound_slash_commands: bool = True, recreate_changed_slash_commands: bool = True, slash_commands_only: bool = False, default_enabled_guilds: typing.Optional[typing.Union[int, typing.Iterable[int]]] = None, **kwargs: hikari.UndefinedType) -> None:
-      #  self._token = token
-#        super().__init__(token=secret.token, prefix='1')#token, prefix=prefix, insensitive_commands=insensitive_commands, ignore_bots=ignore_bots, owner_ids=owner_ids, help_class=help_class, delete_unbound_slash_commands=delete_unbound_slash_commands, recreate_changed_slash_commands=recreate_changed_slash_commands, slash_commands_only=slash_commands_only, default_enabled_guilds=default_enabled_guilds, **kwargs)
-
-
 if __name__ == "__main__":
     bot.load_extensions_from('./coms/', must_exist=True)
     bot.run(activity = hikari.Activity(name="Hentai", type=hikari.ActivityType.WATCHING), status=presences.Status.DO_NOT_DISTURB)
