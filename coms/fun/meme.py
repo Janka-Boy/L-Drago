b444328b35aa66e57b824034e134499e1ece7a0b
Janka <jankadiscord@gmail.com>
2021-11-05 21:46:33 +0200

    nothing changes

---
+++ coms/fun/meme.py
@@ -0,0 +1,14 @@
+import hikari
+import lightbulb
+import requests
+
+class meme(lightbulb.SlashCommand):
+    description: str = "This command does the magic"
+    async def callback(self, ctx: lightbulb.SlashCommandContext) -> None:
+        
+
+
+
+
+def load(bot: lightbulb.Bot) -> None:
+    bot.add_slash_command(meme)
