dba4fbc9e76cba1f9d3a7ec5193ef6557b08b05a
Janka <jankadiscord@gmail.com>
2021-11-05 21:54:45 +0200

    finally

--- coms/fun/meme.py
+++ coms/fun/meme.py
@@ -5,7 +5,7 @@ import requests
 class meme(lightbulb.SlashCommand):
     description: str = "This command does the magic"
     async def callback(self, ctx: lightbulb.SlashCommandContext) -> None:
-        
+        pass
 
 
 
