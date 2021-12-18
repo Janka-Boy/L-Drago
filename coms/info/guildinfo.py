64bb14e162caf9fea752e07e49678add7e82ad2a
Janka <jankadiscord@gmail.com>
2021-12-18 19:01:26 +0200

    clean up

--- coms/info/guildinfo.py
+++ coms/info/guildinfo.py
@@ -11,7 +11,9 @@ class guild(lightbulb.SlashCommand):
         guild_createdAt= int(guild.created_at.timestamp())
 
         guild_embed = hikari.Embed(description=f'**{guild}**', color='#5930D0', timestamp=datetime.now().astimezone())
-        hikari.Embed.add_field(self=guild_embed, name='♤ Guild info:', value=f'**Guild name:** {guild}\n**ID:** {guild.id}\n**Guild was made:** <t:{guild_createdAt}:d> (<t:{guild_createdAt}:R>)\n**Members:** {guild.member_count}')
+        hikari.Embed.add_field(self=guild_embed, 
+        name='♤ Guild info:', 
+        value=f'**Guild name:** {guild}\n**ID:** {guild.id}\n**Guild was made:** <t:{guild_createdAt}:d> (<t:{guild_createdAt}:R>)\n**Members:** {guild.member_count}')
         guild_embed.set_thumbnail(guild.icon_url)
 
         await ctx.respond(guild_embed)
