87e3009aee7ddb97da8e78692931235712a69685
Janka <jankadiscord@gmail.com>
2021-12-18 19:00:02 +0200

    clean up `info` command

--- coms/info/botinfo.py
+++ coms/info/botinfo.py
@@ -7,9 +7,20 @@ class info(lightbulb.SlashCommand):
     description: str = "Get info about bot"
 
     async def callback(self, ctx: lightbulb.SlashCommandContext) -> None:
-        info_embed = hikari.Embed(description='**Generic bot with no purpose in discord community**\n Creator of bot is JanKa#7069\n Big thanks Kingdom_Rush_Pro#1771 for helping with project ', colour='#5930D0', title='**L-Drago**')
+        info_embed = hikari.Embed(
+        description=
+        """
+        **Bot: L-Drago**
+        Generic bot with no purpose in discord community. Using this bot as my test subject. :cold_face:
+
+        **Team and technology:**
+        Making in `Python`. Using `hikari` and `lightbulb` modules.
+        Big thanks `Kingdom_Rush_Pro#1771` for helping with project.
+        """,
+        colour='#5930D0', 
+        title='**Bot creator: JanKa**')
         #Maybe in future, was cringing too hard to include thumbnail info_embed.set_thumbnail('')
-        hikari.Embed.set_footer(info_embed, text='Creator JanKa#7069', icon='https://cdn.discordapp.com/attachments/906936324630667264/906937904633376848/janka_glasses.png')
+        hikari.Embed.set_footer(info_embed, text='Creator: JanKa#7069', icon='https://cdn.discordapp.com/attachments/906936324630667264/921801357017620500/discord_pfp.png')
 
         await ctx.respond(info_embed)
 
